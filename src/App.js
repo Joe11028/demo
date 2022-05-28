99a55021b43a86e652dcc645c3ddec0e7d0fa473
HsiungJoe <53402869+Joe11028@users.noreply.github.com>
2022-05-28 20:54:26 +0800

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SignIn from './Pages/SignIn';
+import Welcome from './Pages/Welcome';
+import Orders from './Pages/Orders';
+import Products from './Pages/Products';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/Welcome/" element={<Welcome/>} />
+          <Route path="/Welcome/Products" element={<Products/>} />
+          <Route path="/Welcome/Orders" element={<Orders/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const Home = () => (
+  <SignIn />
+);
